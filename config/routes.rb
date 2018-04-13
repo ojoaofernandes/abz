6afd99c9d7e3684741be8c636d2c39260fe65bf7
João Fernandes <ojoaofernandes@gmail.com>
2018-04-12 21:55:14 -0300

    Customizes login and signin pages.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations',
+    passwords: 'users/passwords'
+  }
 
-  devise_for :users
   devise_scope :user do
-    root :to => 'users/sessions#new'
+   root :to => 'users/sessions#new'
   end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
