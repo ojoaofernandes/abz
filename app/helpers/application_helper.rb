ceb4340d8a2fc82f846bf9e66c4d92870ef4baf8
João Fernandes <ojoaofernandes@gmail.com>
2018-04-12 21:53:35 -0300

    Implements layout helpers.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def nav_link(description, path)
+    content_tag(:li, class: 'nav-item') do
+      link_to description, path, class: 'nav-link'
+    end
+  end
+
+  def title title
+    content_tag :div, class: 'page-header' do
+      content_tag :h2 do
+        title
+      end
+    end
+  end
 end
