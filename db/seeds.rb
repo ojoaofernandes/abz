3ba0a85ec49966e5b32e1bcf97f838f952e60ac3
João Fernandes <ojoaofernandes@gmail.com>
2018-04-13 20:01:47 -0300

    Populates seeds with more users.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,56 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create(email: 'admin@abz.com', name: 'administrator', 
-                password: '123456', password_confirmation: '123456', 
-                role: 'admin', phone: '45999999999')
+User.create(
+  email: 'admin@abz.com',
+  name: 'administrator',
+  password: '123456',
+  password_confirmation: '123456',
+  role: 'admin',
+  phone: '45999999999'
+)
+
+User.create(
+  email: 'alice@alunos.utfpr.edu.br',
+  name: 'Alice',
+  password: '123456',
+  password_confirmation: '123456',
+  role: 'user',
+  phone: '45999999999'
+)
+
+User.create(
+  email: 'bob@alunos.utfpr.edu.br',
+  name: 'Bob',
+  password: '123456',
+  password_confirmation: '123456',
+  role: 'user',
+  phone: '45999999999'
+)
+
+User.create(
+  email: 'charlie@alunos.utfpr.edu.br',
+  name: 'Charlie',
+  password: '123456',
+  password_confirmation: '123456',
+  role: 'user',
+  phone: '45999999999'
+)
+
+User.create(
+  email: 'david@alunos.utfpr.edu.br',
+  name: 'David',
+  password: '123456',
+  password_confirmation: '123456',
+  role: 'user',
+  phone: '45999999999'
+)
+
+User.create(
+  email: 'erin@alunos.utfpr.edu.br',
+  name: 'Erin',
+  password: '123456',
+  password_confirmation: '123456',
+  role: 'user',
+  phone: '45999999999'
+)
