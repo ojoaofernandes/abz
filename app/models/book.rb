841a25f394965c5f515189ba2ecfed9eccd91088
Uesley S. Souza <uesleysilva@alunos.utfpr.edu.br>
2018-04-17 16:15:23 -0300

    Set up booker uploader configuration

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,6 @@
-class Book < ApplicationRecord
+class Book < ActiveRecord::Base
     belongs_to :discipline
     belongs_to :user
-    
+    mount_uploader :image, ImageUploader
     validates_presence_of :title, :author
 end
