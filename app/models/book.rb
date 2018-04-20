5d2f279e4ff62944a13bec90250fa25db2b76ff2
Uesley S. Souza <uesleysilva@alunos.utfpr.edu.br>
2018-04-19 21:01:00 -0300

    The relationship of reserves and loans.

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,7 @@
 class Book < ActiveRecord::Base
     belongs_to :discipline
     belongs_to :user
+    has_many :bookings
     mount_uploader :image, ImageUploader
     validates_presence_of :title, :author
 end
